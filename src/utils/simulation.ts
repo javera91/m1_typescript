aee2bca0b32cca86718683d40a638078cd4026bb
javera91 <146044960+javera91@users.noreply.github.com>
2024-04-30 20:42:11 -0500

    c1
    c1

--- src/utils/simulation.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/simulation.ts
-
-function simulateTime(time: number): void {
-    console.log(`Simulating ${time} units of time...`);
-    // Implementar lógica de simulación aquí
-  }
-  
-  export { simulateTime };
-  
--- src/utils/user.input.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/user.input.ts
-
-function getUserInput(): string {
-    // Simular la obtención de entrada del usuario
-    return 'User input';
-  }
-  
-  export { getUserInput };
-  
